610fc11ff364addfca4c74ea2776b5fc13786083
Gabriel Porta <gabrielportabaldonedo@gmail.com>
2021-11-04 20:11:02 -0300

    commit

---
+++ Modulo 4/RescateAnimal/backend/routes/admin/login.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var usariosModel = require('./../../models/usuariosModel');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin/login', {
+        layout: '/admin/layout'
+  });
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    var usuario = req.body.usuario;
+    var password = req.body.password;
+
+    var data = await usuariosModel.getUserByUsernameAndPassword(usuario, password);
+
+    if (data != undefined) {
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+module.exports = router;
